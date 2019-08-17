355090f3e25e561c7e3c4c32cc39b14115d6a54c
Prathmesh Saini <prathmesh.saini_cs18@gla.ac.in>
2019-08-17 22:05:48 +0530

    variable declaration calculation print the output

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -5,16 +5,17 @@
 package main;
 
 import java.util.Scanner;
-// uncomment the above line to use input in your program.
+
 
 public class Gravity {
 
 	public static void main(String[] args) {
-		/* TODO 2:
-		 *   a. Declare all your necessary variables inside main method.
-		 *   b. Declare a constant for 'gravity', assume the value to be 9.8
-		 *   c. Calculate the result and print it to the screen.
-		 * */
+			Scanner s = new Scanner(System.in);
+			double time = s.nextDouble();
+			double speed = 9.8*time;
+			double distance = 0.5*9.8*time*time;
+			String str = String.format("%.15f",speed);
+		System.out.println("The speed of the object at "+time+" seconds after its release is "+str+" and the distance the object has travelled in the "+time+" seconds after the relase is "+distance);
 	}
 	// P.S: Refer to the README.md file for the problem statement, input & output.
 }
